9496e6d64918abaddedbc9aa85703ca9bdbd7d6a
NatChung <nat.chung1@gmail.com>
2017-02-17 14:03:22 +0800

    Initial commit

---
+++ SimpleFactory/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleFactory
+{
+	interface IOperator
+	{
+		double getResult(double operand1, double operand2);
+	}
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
