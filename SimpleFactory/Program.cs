bb1018926a0af36d5f37f6e9dc4b35e4b3e938c1
NatChung <nat.chung1@gmail.com>
2017-02-17 14:23:27 +0800

    [A]Added operator simple factory

--- SimpleFactory/Program.cs
+++ SimpleFactory/Program.cs
@@ -7,11 +7,66 @@ namespace SimpleFactory
 		double getResult(double operand1, double operand2);
 	}
 
+	class Adder : IOperator
+	{
+		double IOperator.getResult(double operand1, double operand2)
+		{
+			return operand1 + operand2;
+		}
+	}
+
+	class Subtractor : IOperator
+	{
+		public double getResult(double operand1, double operand2)
+		{
+			return operand1 - operand2;
+		}
+	}
+
+	class Multiplier : IOperator
+	{
+		public double getResult(double operand1, double operand2)
+		{
+			return operand1 * operand2;
+		}
+	}
+
+	class Divider : IOperator
+	{
+		public double getResult(double operand1, double operand2)
+		{
+			return operand1 / operand2;
+		}
+	}
+
+	class OperatorSimpleFactory
+	{
+		public static IOperator createOperator(string type)
+		{
+			switch (type)
+			{
+				case "+":
+					return new Adder();
+				case "-":
+					return new Subtractor();
+				case "*":
+					return new Multiplier();
+				case "/":
+					return new Divider();
+				default:
+					return null;
+			}
+		}
+	}
+
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Add:" + OperatorSimpleFactory.createOperator("+").getResult(3, 6));
+			Console.WriteLine("Substract:" + OperatorSimpleFactory.createOperator("-").getResult(3, 6));
+			Console.WriteLine("Multiple:" + OperatorSimpleFactory.createOperator("*").getResult(3, 6));
+			Console.WriteLine("Divider:" + OperatorSimpleFactory.createOperator("/").getResult(3, 6));
 		}
 	}
 }
